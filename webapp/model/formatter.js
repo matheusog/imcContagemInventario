310ea0be893f078a9adfa93b292f91fc869b5087
Matheus Oliveira Goulart <matheusog@MacBook-Pro-de-Matheus.local>
2019-09-23 13:39:41 -0300

    Commit Matheus 29/09 v3

---
+++ webapp/model/formatter.js
@@ -0,0 +1,34 @@
+sap.ui.define([], function() {
+	"use strict";
+
+	return {
+		
+		formatCentro : function(sPlant, sPlantName) {
+			return sPlantName && sPlantName.trim() !== "" ?
+				`${sPlant} (${sPlantName})` : sPlant;
+		}, 
+		
+		formatStringOdataDate :function(sOdataDate) {
+			if(sOdataDate) {
+				var dDate		= eval('new ' + sOdataDate.replace(/\//g, ''));
+				return dDate;
+			}
+		},
+		
+		formatStringOdataDateDisplay : function(sOdataDate) {
+			//var dDate		= eval('new ' + sOdataDate.replace(/\//g, ''));
+			var dDate = this.formatStringOdataDate(sOdataDate);
+			
+			var oFormatOptions = {
+				style: "medium", 
+				strictParsing: true
+//				pattern: "dd.MM.DDDD"
+			}
+			
+			var oDateFormat 	= sap.ui.core.format.DateFormat.getDateInstance(oFormatOptions);
+			var sDateFormatted	= oDateFormat.format(dDate);
+			
+			return sDateFormatted;
+		}
+	};
+});
